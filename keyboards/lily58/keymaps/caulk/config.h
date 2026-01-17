f6de838635e135d3d50d052817f4ffc9757c8669
cau1k <zero@composer.group>
2026-01-16 20:36:49 -0500

    awesome

--- keyboards/lily58/keymaps/caulk/config.h
+++ keyboards/lily58/keymaps/caulk/config.h
@@ -29,7 +29,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
+#define TAPPING_TERM_PER_KEY
+
+#ifdef RGB_MATRIX_SPLIT
+#    undef RGB_MATRIX_SPLIT
+#endif
+#define RGB_MATRIX_SPLIT { 27, 27 }
 
 // Underglow
 /*
--- keyboards/lily58/keymaps/caulk/keymap.c
+++ keyboards/lily58/keymaps/caulk/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Ctrl | A/Ctl| S/Gui| D/Alt| F/Sft|   G  |-------.    ,-------|   H  | J/Sft| K/Alt| L/Gui| ;/Ctl|  '   |
+     * | Ctrl |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   `-------------------''-------'           '------''--------------------'
      */
 
-    [_BASE] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_NAV), KC_LGUI, KC_LALT, LT(_NAV, KC_SPC), LT(_SYM, KC_ENT), KC_BSPC, KC_RGUI, MO(_SYM)),
+    [_BASE] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_NAV), KC_LGUI, KC_LALT, LT(_NAV, KC_SPC), LT(_SYM, KC_ENT), KC_BSPC, KC_RGUI, MO(_SYM)),
 
     /* NAV */
     [_NAV] = LAYOUT(    OSM(MOD_LALT), KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,                           KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,         KC_TRNS,
@@ -80,6 +80,17 @@ bool caps_word_press_user(uint16_t keycode) {
             return true;
         default:
             return false;
+    }
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(_NAV, KC_SPC):
+            return 220;
+        case LT(_SYM, KC_ENT):
+            return 200;
+        default:
+            return TAPPING_TERM;
     }
 }
 
