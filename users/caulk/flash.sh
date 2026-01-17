dc5d1245adf5dd4165d9e865053f975f3eecf8d3
cau1k <zero@composer.group>
2026-01-16 20:22:21 -0500

    awesome

--- users/caulk/flash.sh
+++ users/caulk/flash.sh
@@ -71,7 +71,7 @@ flash_once() {
 }
 
 cd "$ROOT_DIR"
-qmk compile -kb "$KEYBOARD" -km "$KEYMAP" MASTER_SIDE=left POINTING_DEVICE_POSITION=left
+MASTER_SIDE=left POINTING_DEVICE_POSITION=left qmk compile -kb "$KEYBOARD" -km "$KEYMAP"
 
 if [[ ! -f "$UF2" ]]; then
   echo "UF2 not found at ${UF2}"
