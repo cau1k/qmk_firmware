0c427c9dea4890acc52b2cc4e288b4ba6c25b183
cau1k <zero@composer.group>
2026-01-19 01:10:09 -0500

    feat updated

--- keyboards/lily58/keymaps/caulk/keymap.c
+++ keyboards/lily58/keymaps/caulk/keymap.c
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├───────────────┼───┼──────┼──────┼──────┼─────┼──────────┐   ┌──────────┼─────┼──────┼──────┼──────┼───┼─────────────┤
 //    │     lsft      │ z │  x   │  c   │  v   │  b  │ MO(_SYM) │   │ MO(_NAV) │  n  │  m   │  ,   │  .   │ / │    rsft     │
 //    └───────────────┴───┼──────┼──────┼──────┼─────┼──────────┘   └──────────┼─────┼──────┼──────┼──────┼───┴─────────────┘
-//                        │ lctl │ lgui │ lalt │ spc │                         │ ent │ bspc │ rgui │ lctl │
+//                        │ lctl │ lgui │ lalt │ spc │                         │ ent │ bspc │ lgui │ lctl │
 //                        └──────┴──────┴──────┴─────┘                         └─────┴──────┴──────┴──────┘
 [_BASE] = LAYOUT(
   KC_ESC           , KC_1 , KC_2    , KC_3    , KC_4    , KC_5   ,                           KC_6   , KC_7    , KC_8    , KC_9    , KC_0    , KC_GRV           ,
   LT(_SYM, KC_TAB) , KC_Q , KC_W    , KC_E    , KC_R    , KC_T   ,                           KC_Y   , KC_U    , KC_I    , KC_O    , KC_P    , LT(_SYM, KC_MINS),
   MO(_NAV)         , KC_A , KC_S    , KC_D    , KC_F    , KC_G   ,                           KC_H   , KC_J    , KC_K    , KC_L    , KC_SCLN , LT(_NAV, KC_QUOT),
   KC_LSFT          , KC_Z , KC_X    , KC_C    , KC_V    , KC_B   , MO(_SYM) ,     MO(_NAV) , KC_N   , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT          ,
-                            KC_LCTL , KC_LGUI , KC_LALT , KC_SPC ,                           KC_ENT , KC_BSPC , KC_RGUI , KC_LCTL
+                            KC_LCTL , KC_LGUI , KC_LALT , KC_SPC ,                           KC_ENT , KC_BSPC , KC_LGUI , KC_LCTL
 ),
 
 //    ┌───────────────┬─────┬─────┬─────┬─────┬─────┐               ┌────────────┬──────┬──────┬────────────┬─────┬─────┐
@@ -68,19 +68,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ┌─────────┬─────┬─────┬─────┬─────┬─────┐               ┌─────┬─────┬─────┬─────┬─────┬─────┐
 //    │ CW_TOGG │     │     │     │     │     │               │     │     │     │     │     │     │
 //    ├─────────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │   `     │  1  │  2  │  3  │  4  │  5  │               │  6  │  7  │  8  │  9  │  0  │  -  │
+//    │    `    │  1  │  2  │  3  │  4  │  5  │               │  6  │  7  │  8  │  9  │  0  │  -  │
 //    ├─────────┼─────┼─────┼─────┼─────┼─────┤               ├─────┼─────┼─────┼─────┼─────┼─────┤
-//    │   ~     │  !  │  @  │  #  │  $  │  %  │               │  ^  │  &  │  *  │  (  │  )  │  _  │
+//    │    ~    │  !  │  @  │  #  │  $  │  %  │               │  ^  │  &  │  *  │  (  │  )  │  _  │
 //    ├─────────┼─────┼─────┼─────┼─────┼─────┼─────┐   ┌─────┼─────┼─────┼─────┼─────┼─────┼─────┤
-//    │         │  ^  │  -  │  +  │  =  │  \  │     │   │     │  [  │  ]  │  {  │  }  │  /  │     │
+//    │         │  ^  │  -  │  +  │  =  │  \  │     │   │     │     │  {  │  }  │  [  │  ]  │     │
 //    └─────────┴─────┼─────┼─────┼─────┼─────┼─────┘   └─────┼─────┼─────┼─────┼─────┼─────┴─────┘
-//                    │     │     │     │     │               │     │  \| │     │     │
+//                    │     │     │     │     │               │     │  \  │     │     │
 //                    └─────┴─────┴─────┴─────┘               └─────┴─────┴─────┴─────┘
 [_SYM] = LAYOUT(
   CW_TOGG , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS,
   KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                         KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS,
   KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC ,                         KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_UNDS,
-  KC_TRNS , KC_CIRC , KC_MINS , KC_PLUS , KC_EQL  , KC_BSLS , KC_TRNS ,     KC_TRNS , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR , KC_SLSH , KC_TRNS,
+  KC_TRNS , KC_CIRC , KC_MINS , KC_PLUS , KC_EQL  , KC_BSLS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_LCBR , KC_RCBR , KC_LBRC , KC_RBRC , KC_TRNS,
                       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                         KC_TRNS , KC_BSLS , KC_TRNS , KC_TRNS
 ),
 
